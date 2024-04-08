020595ff433550bab9fb25c2dde038b2c6b30881
ThomasC12000 <146649519+ThomasC12000@users.noreply.github.com>
2024-04-08 09:10:23 +0000

    add binary_tree_print.c file

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,5 +1,13 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_node - Creates a binary tree node.
+ *
+ * @parent: pointer to the parent node.
+ * @value: integer stored in the node.
+ *
+ * Return: a pointer to the new created binary tree node.
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 
