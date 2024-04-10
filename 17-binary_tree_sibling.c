3f8b81e986b13cba42e42af1bf20ae48f6c32e69
ThomasC12000 <146649519+ThomasC12000@users.noreply.github.com>
2024-04-10 07:23:58 +0000

    update

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -15,9 +15,9 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 		return (node->parent->right);
 
 	else if (node->parent->right == node)
-		return (node ->parent->left);
+		return (node->parent->left);
 
 	else
 
-	return	(NULL);
+	return (NULL);
 }
